8f57cc828b141edc6f6523c64650f92e7795fd11
mohit <bhadanimohit@1997@gmail.com>
2024-04-13 18:51:05 +0530

    added war name

--- src/test/java/in/ineuron/JenkinsExample1ApplicationTests.java
+++ src/test/java/in/ineuron/JenkinsExample1ApplicationTests.java
@@ -1,28 +1,14 @@
 package in.ineuron;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import junit.framework.Assert;
-
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -40,7 +26,7 @@ class JenkinsExample1ApplicationTests {
 
 	@Test
 	void CICDDemoControllerTest() throws Exception {
-		Assert.assertEquals(true, true);
+		assertEquals(true, true);
 	}
 
 }
